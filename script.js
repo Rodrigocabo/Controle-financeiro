4baad23287fcde55980bf8e350bfdf60e0eb9bf5
rodrigo carvalho <rodrigocarvalho00525@gmail.com>
2021-09-30 20:51:08 -0300

    controle financeiro

--- script.js
+++ script.js
@@ -2,5 +2,16 @@ const dummyTransactions =[
 {id: 1, name: "bolo de brigadeiro", amount: -20 },
 {id: 2, name: "salario," amount: 300},
 {id: 3, name: "torta de frango", amount: -10},
-{id: 1, name: "bolo de brigadeiro", amount: -20},
+{id: 4, name:"camisa" amount: 150 },
 ]
+
+const addTransactionIntDOM = transaction => {
+const operator = transaction.amount < 0 ? '-' : '+'
+console.log(operator);
+
+</*li class="minus">
+    salario <span>-$500</span><button class="delete-btn">x</button>
+</li> */}
+}
+
+addTransactionIntDOM(dummyTransactions)
