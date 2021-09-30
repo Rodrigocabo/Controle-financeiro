3ad5d09a9dbaad5e3c3cd882a17f215396d90d9a
rodrigo carvalho <rodrigocarvalho00525@gmail.com>
2021-09-30 09:14:55 -0300

    controle financeiro

---
+++ script.js
@@ -0,0 +1,6 @@
+const dummyTransactions =[
+{id: 1, name: "bolo de brigadeiro", amount: -20 },
+{id: 2, name: "salario," amount: 300},
+{id: 3, name: "torta de frango", amount: -10},
+{id: 1, name: "bolo de brigadeiro", amount: -20},
+]
